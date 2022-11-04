32ff562f1a5c8c2aab1b439032757274663fa0fa
RaimondnicusorBulai <Condivisionedatiraimond@gmail.com>
2022-11-04 11:45:15 +0100

    Finish

--- src/main/java/PrimiPassi/First/API2/FirstApi02Application.java
+++ src/main/java/PrimiPassi/First/API2/FirstApi02Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstApi02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstApi02Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApi02Application.class, args);
+    }
 
 }
---
+++ src/main/java/PrimiPassi/First/API2/controller/FactorialController.java
@@ -0,0 +1,25 @@
+package PrimiPassi.First.API2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/factorial")
+public class FactorialController {
+
+    @GetMapping("/{n}")
+    public int factorial(@PathVariable int n) {
+        int factorial = 1;
+        if (n < 0) {
+            factorial = 0;
+        } else {
+            for (int i = 0; i < n; ++i) {
+                factorial *= (i + 1);
+            }
+        }
+        return factorial;
+    }
+
+}
